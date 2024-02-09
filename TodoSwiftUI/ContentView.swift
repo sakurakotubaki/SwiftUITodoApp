1c5c6cb24250231c077888c0ce1066e5b72a41ab
JunichiHashimoto <gitonly543@gmail.com>
2024-02-09 19:43:24 +0900

    Initial Commit

---
+++ TodoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoSwiftUI
+//
+//  Created by 橋本純一 on 2024/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by 橋本純一 on 2024/02/09.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
